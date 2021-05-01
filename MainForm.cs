597f51a6469958293ed49711f6edf2088665c024
10Johannes <72289656+10Johannes@users.noreply.github.com>
2021-05-01 12:18:05 +0800

    Add files via upload
    Created design for my calculator application.

---
+++ MainForm.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: John Rey B. Macascas
+ * Date: 5/1/2021
+ * Time: 10:31 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+	/// <summary>
+	/// Create my first calculator application in Object-Oriented Programming.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button0Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button6Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button7Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button8Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button9Click(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonPointClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonAddClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonMinusClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonMultiplyClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonDivideClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonEXPClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonModuloClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonClearClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonCEClick(object sender, EventArgs e)
+		{
+	
+		}
+		void ButtonEqualClick(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
